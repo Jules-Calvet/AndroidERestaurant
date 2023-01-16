9d7dbd9af6b5414f693d6f37373193d3f520a1a3
Jules Calvet <jules.calvet@isen.yncrea.fr>
2023-01-16 11:27:24 +0100

    toast example

--- app/src/main/java/fr/isen/calvet/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/calvet/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,17 @@ package fr.isen.calvet.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val action = findViewById<Button>(R.id.button1)
+        action.setOnClickListener{
+            Snackbar.make(it, "Entrées", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
